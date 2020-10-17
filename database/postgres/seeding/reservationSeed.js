a8aef01818f9be803fa4876077be3a8b30a35f5b
Haneen Amro <heno.amro@gmail.com>
2020-10-17 10:20:09 -0700

    fixed the data generator

--- database/postgres/seeding/reservationSeed.js
+++ database/postgres/seeding/reservationSeed.js
@@ -57,3 +57,4 @@ const seed = async () => {
 }
 
 seed();
+
--- database/postgres/seeding/userTableSeed.js
+++ database/postgres/seeding/userTableSeed.js
@@ -11,17 +11,13 @@ const seed = async () => {
   const createReviewTableCSV = async () => {
     writer.pipe(fs.createWriteStream('UsrTable.csv'));
     for (var i = 0; i < 1000000; i++) {
-
-      for (var j = 0; j < ( 2 + Math.floor(Math.random() * 2)); j++) {
-
         writer.write({
-          usrId : j+1,
+          usrId : i+1,
           firstName: faker.name.firstName(),
           lastName: faker.name.lastName(),
           email: faker.internet.email(),
           phoneNumber: faker.phone.phoneNumberFormat(),
         });
-      }
     }
     writer.end();
     console.log('The data generated');
